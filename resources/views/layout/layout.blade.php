c3ba6bf8593c4bf3307fa90d626e99afc364a320
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-03-03 00:07:10 +0100

    landing page

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,20 @@
+@include('layout.header')
+
+<div x-data="setup()" :class="{ 'dark': isDark }">
+    <div class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-700 text-black dark:text-white">
+        <section class="flex w-full h-full">
+            <!-- Sidebar -->
+            @include('layout.side-bar')
+
+            <!-- container -->
+            <main class="h-screen w-full overflow-auto" style="background-image: url('http://127.0.0.1:8000/storage/images/landingpageimage.jpg');">
+                @include('layout.success')
+                @yield('content')
+            </main>
+
+        </section>
+
+    </div>
+</div>
+@include('layout.footer')
+@include('js-script.script')
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-//    return view('welcome');
-    return 'hello';
+    return view('landing-page');
 });
