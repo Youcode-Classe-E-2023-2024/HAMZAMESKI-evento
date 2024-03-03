95842cb4817d055b9b275ed3acd5faf45cf27248
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-03-03 13:16:15 +0100

    layout

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,20 +1,16 @@
 @include('layout.header')
-
-<div x-data="setup()" :class="{ 'dark': isDark }">
-    <div class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white dark:bg-gray-700 text-black dark:text-white">
-        <section class="flex w-full h-full">
-            <!-- Sidebar -->
-            @include('layout.side-bar')
-
-            <!-- container -->
-            <main class="h-screen w-full overflow-auto" style="background-image: url('http://127.0.0.1:8000/storage/images/landingpageimage.jpg');">
-                @include('layout.success')
-                @yield('content')
-            </main>
-
-        </section>
-
+<div class="container">
+    @include('layout.side-bar')
+    <!-- ========================= Main ==================== -->
+    <div class="main">
+        <div class="topbar">
+            <div class="toggle">
+                <ion-icon name="menu-outline"></ion-icon>
+            </div>
+        </div>
+        <div>
+            @yield('content')
+        </div>
     </div>
 </div>
 @include('layout.footer')
-@include('js-script.script')
