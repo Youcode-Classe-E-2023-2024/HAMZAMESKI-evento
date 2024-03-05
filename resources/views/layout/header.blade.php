a3982cf1ed6f82e67f8f44f0c4195102aeaf270e
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-03-05 09:45:58 +0100

    organizer table

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -7,8 +7,18 @@
 
     <title>{{ config('app.name') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    {{--  ionicons CDN  --}}
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    {{--  bootsrap CDN  --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+    {{-- jquery CDN --}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    {{-- CHART.js CDN --}}
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     @livewireStyles
 </head>
--- resources/views/manage-events-popup/edit-delete-form.blade.php
+++ resources/views/manage-events-popup/edit-delete-form.blade.php
@@ -1,36 +1,46 @@
 {{-- update rss item --}}
-<div wire:ignore.self class="modal fade" id="updateRssItemModal" tabindex="-1" aria-labelledby="updateRssItemModalLabel"
+<div wire:ignore.self class="modal fade" id="updateEventModal" tabindex="-1" aria-labelledby="updateEventModalLabel"
      aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header flex justify-between">
-                <h5 class="modal-title" id="updateRssItemModalLabel">Edit Student</h5>
+                <h5 class="modal-title" id="updateEventModalLabel">Edit Student</h5>
                 <button type="button" data-bs-dismiss="modal" wire:click="closeModal"
                         aria-label="Close">
                     <div class="h-[20px] w-[20px]" style="background-image: url('http://127.0.0.1:8000/storage/images/close-outline.svg'); background-size: cover; background-position: center; "></div>
                 </button>
             </div>
-            <form wire:submit.prevent="updateRssItem">
+            <form wire:submit.prevent="updateEvent">
                 <div class="modal-body">
                     <div class="mb-3">
-                        <label>RSS item Name</label>
+                        <label>Event Name</label>
                         <input type="text" wire:model="name" class="form-control">
                         @error('name') <span class="text-danger">{{ $message }}</span> @enderror
                     </div>
                     <div class="mb-3">
-                        <label>RSS item Category</label>
+                        <label>Event description</label>
+                        <input type="text" wire:model="description" class="form-control">
+                        @error('description') <span class="text-danger">{{ $message }}</span> @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="date">Event Date</label>
+                        <input type="date" id="date" wire:model="date" class="form-control">
+                        @error('date') <span class="text-danger">{{ $message }}</span> @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label>Event Place</label>
+                        <input type="text" wire:model="place" class="form-control">
+                        @error('place') <span class="text-danger">{{ $message }}</span> @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label>Event Category</label>
                         <input type="text" wire:model="category" class="form-control">
                         @error('category') <span class="text-danger">{{ $message }}</span> @enderror
                     </div>
                     <div class="mb-3">
-                        <label>RSS item Link</label>
-                        <input type="text" wire:model="link" class="form-control">
-                        @error('link') <span class="text-danger">{{ $message }}</span> @enderror
-                    </div>
-                    <div class="mb-3">
-                        <label>RSS item Description</label>
-                        <input type="text" wire:model="description" class="form-control">
-                        @error('description') <span class="text-danger">{{ $message }}</span> @enderror
+                        <label>Available places</label>
+                        <input type="number" wire:model="available_places" class="form-control">
+                        @error('available_places') <span class="text-danger">{{ $message }}</span> @enderror
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -44,20 +54,20 @@
 </div>
 
 {{-- delete rss item Modal--}}
-<div wire:ignore.self class="modal fade" id="deleteRssItemModal" tabindex="-1" aria-labelledby="deleteRssItemModalLabel"
+<div wire:ignore.self class="modal fade" id="deleteEventModal" tabindex="-1" aria-labelledby="deleteEventModalLabel"
      aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header flex justify-between">
-                <h5 class="modal-title" id="deleteRssItemModalLabel">Delete Student</h5>
+                <h5 class="modal-title" id="deleteEventModalLabel">Delete Student</h5>
                 <button type="button" data-bs-dismiss="modal" wire:click="closeModal"
                         aria-label="Close">
                     <div class="h-[20px] w-[20px]" style="background-image: url('http://127.0.0.1:8000/storage/images/close-outline.svg'); background-size: cover; background-position: center; "></div>
                 </button>
             </div>
-            <form wire:submit.prevent="destroyRssItem">
+            <form wire:submit.prevent="destroyEvent">
                 <div class="modal-body">
-                    <h4>Are you sure you want to delete this item ?</h4>
+                    <h4>Are you sure you want to delete this event ?</h4>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn text-white btn hover:bg-gray-600 bg-gray-500 border-0" wire:click="closeModal"
