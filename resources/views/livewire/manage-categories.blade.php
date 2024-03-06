3169cbd5cbe0930daa1c17d49798dd3163fc4a6e
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-03-06 21:31:04 +0100

    manage categories

---
+++ resources/views/livewire/manage-categories.blade.php
@@ -0,0 +1,124 @@
+<div>
+    <h4>
+        <button type="button" class="bg-pink-500 py-3 px-2 text-white mt-8 ml-6 rounded" data-bs-toggle="modal" data-bs-target="#eventModal">
+            Add New Category
+        </button>
+    </h4>
+    <div id="dashboard-container" class=" flex items-center justify-center">
+        @include('manage-categories-popup.add-edit-delete-form')
+
+        <section class="mt-10 w-full">
+            <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
+                <!-- Start coding here -->
+                <div class="backdrop-blur-sm relative shadow-md sm:rounded-lg overflow-hidden border-black border-solid border-[.5px]" style="background-color: rgba(0, 0, 0, 0.403);">
+                    <div class="flex items-center justify-between d p-4">
+                        <div class="flex">
+                            <div class="relative">
+                                <input wire:model.live.debounce.300s="search" type="text"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 pr-4 py-2"
+                                       placeholder="Search" required="">
+                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                         fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                              clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="overflow-x-auto">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-4 py-3">
+                                    name
+                                </th>
+                                <th scope="col" class="px-4 py-3">
+                                    category
+                                </th>
+                                <th scope="col" class="px-4 py-3">
+                                    event date
+                                </th>
+                                <th scope="col" class="px-4 py-3">
+                                    acceptance
+                                </th>
+                                <th scope="col" class="px-4 py-3">
+                                    available places
+                                </th>
+                                <th scope="col" class="px-4 py-3">
+                                    nmb reservations
+                                </th>
+                                <th scope="col" class="px-4 py-3">
+                                    edit
+                                </th>
+                                <th scope="col" class="px-4 py-3">
+                                    delete
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            @foreach($events as $event)
+                                <tr  class="border-b dark:border-gray-700">
+                                    <td scope="col" class="px-4 py-3 text-white" >{{ $event->name }}</td>
+                                    <td scope="col" class="px-4 py-3 text-white">{{ $event->category }}</td>
+                                    <td scope="col" class="px-4 py-3 text-white">{{ $event->date }}</td>
+                                    <td>
+                                        <button type="button" data-bs-toggle="modal" data-bs-target="#updateAcceptanceModal" wire:click="editAcceptance({{$event->id}})" class="btn  {{ $event->acceptance == 'manual'? 'bg-green-500 hover:bg-green-600' : 'bg-purple-500 hover:bg-purple-600' }}">
+                                            {{ $event->acceptance }}
+                                        </button>
+                                    </td>
+                                    <td scope="col" class="px-4 py-3 text-white">{{ $event->available_places }}</td>
+                                    <td scope="col" class="px-4 py-3 text-white">{{ $event->nmb_reservations }}</td>
+                                    <td>
+                                        <button type="button" data-bs-toggle="modal" data-bs-target="#updateEventModal" wire:click="editEvent({{$event->id}})" class="btn btn-primary">
+                                            Edit
+                                        </button>
+                                    </td>
+                                    <td scope="col" class="px-4 py-3 flex items-center justify-end">
+                                        <button type="button" data-bs-toggle="modal" data-bs-target="#deleteEventModal" wire:click="deleteEvent({{$event->id}})" class="btn btn-danger">
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="py-4 px-3">
+                        <div class="flex ">
+                            <div class="flex space-x-4 items-center mb-3">
+                                <label class="w-32 text-sm text-white font-bold">Per Page</label>
+                                <select
+                                    wire:model.live = 'perPage'
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                                    <option value="5">5</option>
+                                    <option value="10">10</option>
+                                    <option value="20">20</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                </select>
+                            </div>
+                        </div>
+                        {{ $events->links() }}
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    {{--jQuery script--}}
+    <script>
+        window.addEventListener('close-modal', event => {
+            $('#eventModal').modal('hide');
+            $('#updateEventModal').modal('hide');
+            $('#deleteEventModal').modal('hide');
+            $('#updateAcceptanceModal').modal('hide');
+        })
+    </script>
+</div>
+
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,22 +2,18 @@
 @section('content')
     <section class="backdrop-blur-sm shadow-md h-full py-6 sm:py-12 dark:text-gray-100" style="background-color: rgba(0, 0, 0, 0.403);">
         <div class="container p-6 mx-auto space-y-8">
-            <div class="space-y-2 text-center">
-                <h2 class="text-3xl font-bold">Events</h2>
-                <p class="font-serif text-sm dark:text-gray-400">Informations</p>
-            </div>
             <div class="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
                 @foreach($events as $event)
                     <article class="flex flex-col dark:bg-gray-900 border-1 border-solid border-purple-500">
-                        <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
+                        <div rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
                             <img  alt="" class="object-cover w-full h-52 dark:bg-gray-500" src="http://127.0.0.1:8000/storage/images/{{ $event->image }}">
-                        </a>
+                        </div>
                         <div class="flex flex-col flex-1 p-6">
-                            <a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-                            <a rel="noopener noreferrer" href="#" class="text-xs tracki uppercase hover:underline dark:text-violet-400">{{ substr($event->description, 0, 40) }}</a>
+                            <div rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></div>
+                            <a rel="noopener noreferrer" href="#" class="text-xs tracki uppercase hover:underline dark:text-violet-400">details</a>
                             <h3 class="flex-1 py-2 text-lg font-semibold leadi">{{ substr($event->name, 0, 20) }}</h3>
                             <div class="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
-                                <span>{{ substr($event->created_at, 0, 20) }}</span>
+                                <span>{{ $event->date }}</span>
                             </div>
                         </div>
                     </article>
