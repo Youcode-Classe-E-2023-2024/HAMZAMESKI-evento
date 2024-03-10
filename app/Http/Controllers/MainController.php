c22cb7b35b8c51f11027a4b4f5b1700a570d2dec
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-03-10 14:22:38 +0100

    pagination / categories filter

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,17 +11,19 @@ class MainController extends Controller
     public function index() {
 
         $black_hover = 'home';
+        $category_hover = 'All';
         $events = Event::latest()->where('is_published', '1')->paginate(10);
         $categories = Category::latest()->get();
 
-        return view('main', compact('black_hover', 'events', 'categories'));
+        return view('main', compact('black_hover', 'category_hover', 'events', 'categories'));
     }
 
     public function category_events (Category $category) {
         $black_hover = 'home';
+        $category_hover = $category->name;
         $events = $category->events()->paginate(10);
         $categories = Category::latest()->get();
 
-        return view('main', compact('black_hover', 'events', 'categories'));
+        return view('main', compact('black_hover', 'category_hover', 'events', 'categories'));
     }
 }
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -17,7 +17,7 @@
 {{--        </a>--}}
 {{--    </div>--}}
     <main class="h-full w-full flex">
-        <section class="flex-grow h-full sm:py-12 dark:text-gray-100">
+        <section class="flex-grow h-full sm:py-12 dark:text-gray-100 mr-[100px]">
             <div class="container px-6 mx-auto space-y-8">
                 <div class="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3">
                     @foreach($events as $event)
@@ -40,15 +40,15 @@
             </div>
         </section>
 
-        <section class="" style="border-left: solid gray 0.1px">
+        <section class="fixed top-0 right-0 bottom-0 bg-black" style="border-left: solid gray 0.1px">
             <p class="font-bold text-white bg-black p-2 px-4 border-b-[1px] border-solid border-pink-500">
                 categories
             </p>
             </p>
-            <div class="h-[94%] w-full overflow-auto flex flex-col" style="background-color: rgba(0, 0, 0, 0.86);">
-                <a href="{{ route('main') }}" class="px-4 py-1 text-gray-300 hover:bg-pink-500 hover:text-black">All</a>
+            <div class="h-[94%] w-full overflow-auto flex flex-col" style="background-color: black;">
+                <a href="{{ route('main') }}" class="px-4 py-1 text-gray-300 hover:bg-pink-500 hover:text-black {{ $category_hover == 'All'? 'bg-pink-600': '' }}">All</a>
                 @foreach($categories as $category)
-                    <a href="{{ route('category-events', $category->id) }}" class="px-4 py-1 text-gray-300 hover:bg-pink-500 hover:text-black"> {{ $category->name }}</a>
+                    <a href="{{ route('category-events', $category->id) }}" class="px-4 py-1 text-gray-300 hover:bg-pink-500 hover:text-black {{ $category_hover == $category->name? 'bg-pink-600': '' }}"> {{ $category->name }}</a>
                 @endforeach
             </div>
         </section>
